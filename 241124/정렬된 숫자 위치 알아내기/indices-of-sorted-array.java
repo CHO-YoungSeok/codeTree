9dd18ce8aa254b8f8310f65436696f8e7bdbce18
ChoYSPRO <150162806+ChoYSPRO@users.noreply.github.com>
2024-11-24 22:23:28 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 259ms, Memory: 14MB, Status: Passed - CodeTree

--- 241124/정렬된 숫자 위치 알아내기/indices-of-sorted-array.java
+++ 241124/정렬된 숫자 위치 알아내기/indices-of-sorted-array.java
@@ -1,60 +1,59 @@
 import java.util.*;
 
-class Num{
-    private static int count = 0;
-    private int value, oriLocation, nextLocation;
 
-    Num(int value){
-        count++;
-        this.value = value;
-        this.oriLocation = count;
+class Num {
+    static int count = 0;
+    private int key, originalLocation, nextLocation;
+
+    Num(int key){
+        count ++;
+        this.key = key;
+        this.originalLocation = count;
     }
     void setNextLocation(int nextLocation){
         this.nextLocation = nextLocation;
     }
-    
-    public static class ComparatorByValue implements Comparator<Num>{
+
+    public static class SortByKey implements Comparator<Num>{
         @Override
         public int compare(Num n1, Num n2){
-            if(n1.value == n2.value){
-                return n1.oriLocation - n2.oriLocation;
+            if(n1.key == n2.key){
+                return n1.originalLocation - n2.originalLocation;
             }
-            return n1.value - n2.value;
+            return n1.key - n2.key;
         }
     }
-
-    public static class ComparatorByOriLocation implements Comparator<Num>{
+    public static class SortByOriginalLocation implements Comparator<Num>{
         @Override
         public int compare(Num n1, Num n2){
-            return n1.oriLocation - n2.oriLocation;
+            return n1.originalLocation - n2.originalLocation;
         }
     }
 
     void printNextLocation(){
-        System.out.print(nextLocation + " ");
+        System.out.print(this.nextLocation + " ");
     }
 }
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        List<Num> ns = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            int value = s.nextInt();
-            ns.add(new Num(value));
-        }
-
-        Collections.sort(ns, new Num.ComparatorByValue());
+        // 여기에 코드를 작성해주세요. 
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Num> arrNum = new ArrayList<>();
         for(int i = 0; i < n;  i++){
-            ns.get(i).setNextLocation(i+1);
+            int key =  scanner.nextInt();
+            arrNum.add(new Num(key));
         }
 
-        Collections.sort(ns, new Num.ComparatorByOriLocation());
-        for(int i = 0; i < n; i++){
-            ns.get(i).printNextLocation();
+        Collections.sort(arrNum, new Num.SortByKey());
+        int i = 0;
+        for(Num num : arrNum){
+            i++;
+            num.setNextLocation(i);
         }
-
-
+        Collections.sort(arrNum, new Num.SortByOriginalLocation());
+        for(Num num : arrNum){
+            num.printNextLocation();
+        }
     }
 }
