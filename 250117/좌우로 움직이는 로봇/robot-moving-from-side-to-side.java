0d52e6362788ac9b2ae0de0d1252e73ab28d46ae
YS <150162806+ChoYSPRO@users.noreply.github.com>
2025-01-17 18:52:43 +0900

    Title: 좌우로 움직이는 로봇, Time: 1030ms, Memory: 52MB, Status: Passed - CodeTree

--- 250117/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250117/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -1,60 +1,61 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner scanner = new Scanner(System.in);
         int n, m;
+        Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         m = scanner.nextInt();
-        int locationA = 0, locationB = 0, preLocationA = 0, preLocationB = 0;
-        int answer = 0;
-        int phageA = 0, phageB = 0;
-        List<Character> dirA = new ArrayList<>();
-        List<Character> dirB = new ArrayList<>();
         List<Integer> timeA = new ArrayList<>();
+        List<Character> dirA = new ArrayList<>();
         List<Integer> timeB = new ArrayList<>();
-        for(int i = 0; i < n ;i++) {
+        List<Character> dirB = new ArrayList<>();
+
+        int phageA = 0, phageB = 0;
+
+        for(int i  = 0; i < n; i++){
             timeA.add(scanner.nextInt());
             dirA.add(scanner.next().charAt(0));
         }
-        for(int i = 0; i < m; i++) {
+        for(int i  = 0; i < m; i++){
             timeB.add(scanner.nextInt());
             dirB.add(scanner.next().charAt(0));
         }
-        
-        while(phageA < n || phageB < m){
-            if(phageA < n) {
-                if(dirA.get(phageA) == 'R')
-                    locationA += timeA.get(phageA);
-                else locationA -= timeA.get(phageA);
-                phageA++;
+        int locationA = 0, locationB = 0;
+        int preLocationA = 0, preLocationB = 0;
+
+        int answer = 0;
+        while(timeA.get(n-1) > 0 || timeB.get(m-1) > 0) {
+            if(timeA.get(n-1) > 0){
+                if(timeA.get(phageA) == 0){
+                    phageA++;
+                }
+                if(dirA.get(phageA) == 'R'){
+                    ++locationA;
+                } else {
+                    --locationA;
+                }
+                timeA.set(phageA, timeA.get(phageA) - 1);
             }
-            if(phageB < m){
-                if(dirB.get(phageB) == 'R') 
-                    locationB += timeB.get(phageB);
-                else 
-                    locationB -= timeB.get(phageB);
-                phageB++;
+            if(timeB.get(m-1) > 0) {
+                if(timeB.get(phageB) == 0){
+                    phageB++;
+                }
+                if(dirB.get(phageB) == 'R'){
+                    locationB ++;
+                } else {
+                    locationB--;
+                }
+                timeB.set(phageB, timeB.get(phageB) - 1);
             }
-            
-            if(locationA < locationB) {
-                if(preLocationA > preLocationB)
+            if(locationA == locationB){
+                if(preLocationA != preLocationB){
                     answer++;
+                }
             }
-            else if(locationA > locationB) {
-                if(preLocationA < preLocationB)
-                    answer++;
-            }
-            else if(locationA == locationB) {
-                if (preLocationA != preLocationB)  
-                    answer++;       
-            }
-            
             preLocationA = locationA;
             preLocationB = locationB;
-            // System.out.print(locationA);
-            // System.out.print(" ");
-            // System.out.println(locationB);
         }
         System.out.println(answer);
     }
