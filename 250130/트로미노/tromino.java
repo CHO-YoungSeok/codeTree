eb666607bc352778a307251a25070709275c1e2c
YS <150162806+ChoYSPRO@users.noreply.github.com>
2025-01-30 16:45:46 +0900

    Title: 트로미노, Time: 363ms, Memory: 22MB, Status: Passed - CodeTree

--- 250130/트로미노/tromino.java
+++ 250130/트로미노/tromino.java
@@ -19,13 +19,13 @@ public class Main {
         int max = 0;
         int sum = 0;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n-2; j++){
+            for(int j = 0; j < m-2; j++){
                 sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
                 if(sum > max) max = sum;
             }
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n -2; j++){
                 sum = grid[j][i] + grid[j+1][i] + grid[j+2][i];
                 if(sum > max) max = sum;
@@ -33,8 +33,8 @@ public class Main {
         }
 
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i + 1 < n && j + 1 < n){
+            for(int j = 0; j < m; j++){
+                if(i + 1 < n && j + 1 < m){
                     sum = grid[i][j] + grid[i+1][j] + grid[i+1][j+1];
                     if(sum > max) max = sum;
                     sum = grid[i][j] + grid[i][j+1] + grid[i+1][j];
