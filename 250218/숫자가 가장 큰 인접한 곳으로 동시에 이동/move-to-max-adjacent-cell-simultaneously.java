962e07658368f1df21a3be6d8664df3586120dca
CYS <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-02-18 15:41:00 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 161ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 250218/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int t = sc.nextInt();
+
+        int[][] grid = new int[n][n];
+        for(int i = 0;  i < n; i++){
+            for(int j = 0 ;  j < n; j++){
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] posX = new int[m];
+        int[] posY = new int[m];
+
+        for(int i = 0; i < m; i++){
+            posX[i] = sc.nextInt();
+            posY[i] = sc.nextInt();
+        }
+        
+        // it help to make clean code.
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
+
+        boolean[] delPos = new boolean[m];
+        for(int phage = 0; phage < t; phage++){
+            for(int i = 0;  i < m && delPos[i] != true; i++){
+                int x = posX[i];
+                int y = posY[i];
+                int big = 0;
+                int dir = 0;
+                for(int j = 0;  j < 4; j++){
+                    if(0 <= x + dx[j] && x + dx[j] < n && 0 <= y + dy[j] && y+dy[j] < n){
+                        if(big < grid[x + dx[j]][y+ dy[j]]) {
+                            big = grid[x + dx[j]][y+ dy[j]];
+                        }
+                    }
+                }
+                for(int j = 0;  j < 4; j++){  // set direction
+                    if(0 <= x + dx[j] && x + dx[j] < n && 0 <= y + dy[j] && y+dy[j] < n){
+                        if(big == grid[x + dx[j]][y+ dy[j]]) {
+                            dir = j;
+                        }
+                    }
+                }
+                posX[i] += dx[dir];
+                posY[i] += dy[dir];
+            }
+            
+            for(int i = 0; i < m && delPos[i] != true; i++){
+                for(int j = i+1; j < m; j++){
+                    if(posX[i] == posX[j] && posY[i] == posY[j]){
+                        delPos[i] = true;
+                        delPos[j] = true;         
+                    }
+                }
+            }
+        }
+
+        int result = 0;
+        for(int i = 0;  i < m; i++){
+            if(delPos[i] != true){
+                result++;
+            }
+        }
+        System.out.println(result);
+
+
+    }
+}
