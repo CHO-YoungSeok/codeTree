7c02f10a1ca140a9daa3fb98330984cd0ad0d335
CYS <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-02-19 21:21:57 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 1524ms, Memory: 36MB, Status: Passed - CodeTree

--- 250219/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
+++ 250219/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer> list = new ArrayList<>();
     static int k, n;
-    
+    static ArrayList<Integer> list = new ArrayList<>();
+
     static void choose(int digit){
-        if(digit == n+1) {
-            for(int i = 0; i < list.size(); i++){
+        if(digit == n+1){
+            for(int i = 0;  i < list.size(); i++){
                 System.out.print(list.get(i) + " ");
             }
             System.out.println();
-            return;
+            return;   
         }
-
         for(int select = 1; select <= k; select++){
             list.add(select);
             choose(digit+1);
-            list.remove(list.size() - 1);
-        }
+            list.remove(list.size() -1);
+        }        
     }
-
+    
     public static void main(String[] args) {
         // Please write your code here.
+
         Scanner sc = new Scanner(System.in);
         k = sc.nextInt();
         n = sc.nextInt();
 
         choose(1);
+
+
+
+
     }
 }
