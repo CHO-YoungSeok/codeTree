873d4481a234ee8dc3f198c1ca6730ee15ef1243
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-11 10:56:53 +0900

    Title: 계단 오르기 2, Time: 190ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 250711/계단 오르기 2/climbing-stairs-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int[][] dp = new int[n + 1][3 + 1];
+        dp[0][0] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = coins[0];
+        dp[2][0] = 0;
+        dp[2][1] = Math.max(dp[1][0] + coins[1], dp[1][1]);
+        dp[2][2] = dp[1][1] + coins[1];
+
+        for (int i = 3; i < n + 1; i++) {
+            for (int j = 1; j < 4; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1] + coins[i - 1], dp[i - 2][j] + coins[i - 1]);
+            }
+        }
+        // for (int[] row : dp) {
+        //     for (int num : row) {
+        //         System.out.print(num + " ");
+        //     }
+        //     System.out.println();
+        // }
+        System.out.println(dp[n][3]);
+        
+    }
+}
