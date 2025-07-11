dfa3be985714dcee3de711834301334b96b40c4f
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-11 16:28:17 +0900

    Title: 가장 많은 데이터, Time: 1165ms, Memory: 43MB, Status: Passed - Codetree

---
+++ 250711/가장 많은 데이터/most-frequent-data.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+        // Please write your code here.
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        int max = 0;
+        for (int num : map.values()) {
+            max = Math.max(num, max);
+        }
+
+
+        System.out.println(max);
+    }
+}
