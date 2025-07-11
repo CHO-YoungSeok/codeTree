72bb8a287da36d71fa73ca891e90acabac7b7f06
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-11 13:41:34 +0900

    Title: 계단 오르기 2, Time: 178ms, Memory: 11MB, Status: Passed - Codetree

--- 250711/계단 오르기 2/climbing-stairs-2.java
+++ 250711/계단 오르기 2/climbing-stairs-2.java
@@ -17,12 +17,26 @@ public class Main {
         dp[2][2] = dp[1][1] + coins[1];
 
         for (int i = 3; i < n + 1; i++) {
-            dp[i][0] = dp[i - 2][0] + coins[i - 1];
+            if (i % 2 == 0) {
+                dp[i][0] = dp[i - 2][0] + coins[i - 1];
+            }
+
             for (int  j = 1; j < 4; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1] + coins[i - 1]
-                            , dp[i - 2][j] + coins[i - 1]);
+                if (dp[i - 1][j - 1] == 0){
+                    continue;
+                }
+
+                dp[i][j] = Math.max(dp[i- 1][j - 1], dp[i - 2][j]) + coins[i - 1];
             }
         }
+
+        // for(int[] arr : dp) {
+        //     for (int num : arr) {
+        //         System.out.print(num + " ");
+        //     }
+        //     System.out.println();
+        // }
+
         int result = 0;
         for(int num : dp[n]) {
             result = Math.max(result, num);
