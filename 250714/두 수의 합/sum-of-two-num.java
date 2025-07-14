57678c8d10e56bca727f9292e6f57a42e79d27c0
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-14 14:26:04 +0900

    Title: 두 수의 합, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250714/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i  < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(k - arr[i])) {
+                count += (map.get(arr[i]) * map.get(k - arr[i]));       
+                map.remove(arr[i]);
+                map.remove(k - arr[i]);
+            }
+        }
+
+        System.out.println(count);        
+    }
+}
