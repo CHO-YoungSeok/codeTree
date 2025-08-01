d0b6ffef3c1edbed8a9fef7d086ec4979182e567
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-08-01 10:01:35 +0900

    Title: treeset 기본, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250801/treeset 기본/treeset-basic.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String cmd = sc.next();
+            int num = 0;
+            if (cmd.equals("add")) {
+                num = sc.nextInt();
+                ts.add(num);
+            } else if (cmd.equals("remove")) {
+                num = sc.nextInt();
+                ts.remove(num);
+            } else if (cmd.equals("find")) {
+                num = sc.nextInt();
+                System.out.println(ts.contains(num) ? "true" : "false");
+            } else if (cmd.equals("lower_bound")) {
+                num = sc.nextInt();
+                Integer lower_bound = new Integer(0);
+                lower_bound = ts.ceiling(num);
+                System.out.println(lower_bound == null ? "None" : lower_bound);
+            } else if (cmd.equals("upper_bound")) {
+                 num = sc.nextInt();
+                Integer lower_bound = new Integer(0);
+                lower_bound = ts.ceiling(num);
+                System.out.println(lower_bound == null ? "None" : lower_bound);
+            } else if (cmd.equals("largest")) {
+                
+            } else if (cmd.equals("smallest")) {
+                
+            } else {}
+        }
+
+
+
+    }
+}
