9db0fd46c3a7470ef3b4f589c495f7f270f6c8e2
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-08-29 13:48:07 +0900

    Title: 트로미노, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250829/트로미노/tromino.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] grid = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                if (i - 1 >= 0 && j + 1 < n) {
+                    sum = Math.max(grid[i][j] + grid[i][j+1] + grid[i-1][j], sum);
+                }
+                if (i-1 >= 0 && j-1 >= 0) {
+                    sum = Math.max(grid[i][j] + grid[i-1][j] + grid[i][j-1], sum);
+                }
+                if (i + 1 < n && j-1 >= 0) {
+                    sum = Math.max(grid[i][j] + grid[i+1][j] + grid[i][j-1], sum);
+                }
+                if (i + 1 < n && j + 1 < n) {
+                    sum = Math.max(grid[i][j] + grid[i+1][j] + grid[i][j+1], sum);
+                }
+
+                if (j - 1 >= 0 && j + 1 < n) {
+                    sum = Math.max(grid[i][j] + grid[i][j-1] + grid[i][j+1], sum);
+                }
+                if (i - 1 >= 0 && i + 1 < n) {
+                    sum = Math.max(grid[i][j] + grid[i-1][j] + grid[i+1][j], sum);
+                }
+
+                // System.out.println(i + ", " + j + ": " + sum);
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+
+        System.out.println(maxSum);
+        
+    }
+}
