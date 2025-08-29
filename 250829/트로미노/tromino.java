7aaada27e755d02258463249c58135771a95de8f
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-08-29 14:03:24 +0900

    Title: 트로미노, Time: 248ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250829/트로미노/tromino.java
+++ 250829/트로미노/tromino.java
@@ -16,7 +16,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i - 1 >= 0 && j + 1 < n) {
+                if (i - 1 >= 0 && j + 1 < m) {
                     sum = Math.max(grid[i][j] + grid[i][j+1] + grid[i-1][j], sum);
                 }
                 if (i-1 >= 0 && j-1 >= 0) {
@@ -25,11 +25,11 @@ public class Main {
                 if (i + 1 < n && j-1 >= 0) {
                     sum = Math.max(grid[i][j] + grid[i+1][j] + grid[i][j-1], sum);
                 }
-                if (i + 1 < n && j + 1 < n) {
+                if (i + 1 < n && j + 1 < m) {
                     sum = Math.max(grid[i][j] + grid[i+1][j] + grid[i][j+1], sum);
                 }
 
-                if (j + 1 < n && j + 2 < n) {
+                if (j + 1 < n && j + 2 < m) {
                     sum = Math.max(grid[i][j] + grid[i][j+1] + grid[i][j+2], sum);
                 }
                 if (i + 1 < n && i + 2 < n) {
