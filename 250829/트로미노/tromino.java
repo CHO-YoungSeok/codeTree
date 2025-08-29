ce625819f008f75b8ab3c84abba0a2aca9927c67
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-08-29 14:02:51 +0900

    Title: 트로미노, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250829/트로미노/tromino.java
+++ 250829/트로미노/tromino.java
@@ -15,7 +15,7 @@ public class Main {
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 if (i - 1 >= 0 && j + 1 < n) {
                     sum = Math.max(grid[i][j] + grid[i][j+1] + grid[i-1][j], sum);
                 }
