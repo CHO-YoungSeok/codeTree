a6bc56047fe365ad56a8f1dbaa9323eab26bf692
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-08-30 15:13:33 +0900

    Title: 금 채굴하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250830/금 채굴하기/gold-mining.java
+++ 250830/금 채굴하기/gold-mining.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,13 +10,15 @@ public class Main {
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
         // Please write your code here.
+
         int max = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-
-                for (int k = 0; k <= 2 * (n - 1); k++) {
+                
+                for (int k = 0;  k < 2 * (n-1); k++) {
                     int sum = 0;
+
                     for (int a = 0; a < n; a++) {
                         for (int b = 0; b < n; b++) {
                             if (Math.abs(i - a) + Math.abs(j - b) <= k) {
@@ -25,13 +27,14 @@ public class Main {
                         }
                     }
 
-                    if (Math.pow(k, 2) + Math.pow(k + 1, 2) <= sum * m) {
+                    if (Math.pow(k, 2) + Math.pow((k + 1), 2) <= sum * m) {
                         max = Math.max(max, sum);
                     }
                 }
+
             }
         }
-
         System.out.println(max);
+
     }
 }
