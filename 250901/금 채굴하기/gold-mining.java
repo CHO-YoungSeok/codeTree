c269dcc150b2917aa1f7fe152319dc9ddda32c0a
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-09-01 16:17:26 +0900

    Title: 금 채굴하기, Time: 128ms, Memory: 14MB, Status: Wrong Answer - Codetree

--- 250901/금 채굴하기/gold-mining.java
+++ 250901/금 채굴하기/gold-mining.java
@@ -6,52 +6,47 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] grid = new int[n][n];
-
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n; j++) 
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
-
         // Please write your code here.
-        
+
+
         int[] dx = {1, 1, -1, -1};
         int[] dy = {-1, 1, 1, -1};
 
         int max = 0;
-        int q1 = 0, q2 = 0, q3 = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-
+                
                 int sum = grid[i][j];
 
-                for (int k = 1; k <= 2 * (n - 1); k++) {
+                for (int k = 1; k <= 2 *(n - 1); k++) {
                     int currX = i - k;
                     int currY = j;
-                    for (int p = 0; p < 4; p++) {
-                        for (int a = 0; a < k; a++) {
-                            if (0 <= currX && currX < n
+
+                    for (int dir = 0; dir < 4; dir++) {
+                        for (int step = 0; step < k; step++) {
+                            if (
+                                0 <= currX && currX < n
                                 && 0 <= currY && currY < n
                             ) {
                                 sum += grid[currX][currY];
-                                // System.out.println(currX + ", "+ currY);
                             }
-                            
-                            currX += dx[p];
-                            currY += dy[p];
+
+                            currX += dx[dir];
+                            currY += dy[dir];                            
                         }
                     }
-                    if (sum * m >= (k * k) + ((k+1) * (k+1))) {
+
+                    if (sum * m >= Math.pow(k, 2) + Math.pow((k+1), 2)) {
                         max = Math.max(max, sum);
-                        if (sum == max) {
-                            q1 = i;
-                            q2 = j;
-                            q3 = k;
-                        }
-                    }    
+                    }
                 }
             }
         }
 
         System.out.println(max);
-        // System.out.println(q1 + ", " + q2 + ", " + q3);
+
     }
 }
