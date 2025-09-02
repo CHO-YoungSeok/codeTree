6839432edc68f24971b700027591632136c3562e
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-09-02 13:29:40 +0900

    Title: 금 채굴하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250902/금 채굴하기/gold-mining.java
+++ 250902/금 채굴하기/gold-mining.java
@@ -1,35 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static int getGoldNum(int[][] grid, int r, int c, int k) {
-        if (k == 0) {
-            return grid[r][c];
-        }
-
-        int[] dx = {1, 1, -1, -1};
-        int[] dy = {-1, 1, 1, -1};
-        int currX = r - k;
-        int currY = c;
-        int sum = 0;
-
-        for (int dir = 0; dir < 4; dir++) {
-            for (int step = 0; step < k; step++) {
-                if (
-                    0 <= currX && currX < grid.length
-                    && 0 <= currY && currY < grid.length
-                ) {
-                    sum += grid[currX][currY];
-                }
-
-                currX += dx[dir];
-                currY += dy[dir];
-            }
-        }
-
-        return sum;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -39,16 +10,32 @@ public class Main {
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
         // Please write your code here.
+
+        int[] dx = {1, 1, -1, -1};
+        int[] dy = {-1, 1, 1, -1};
         int max = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int sum = 0;
+                int sum = grid[i][j];
 
-                for (int k = 0; k <= 2 * (n - 1); k++) {
-                    sum += getGoldNum(grid, i, j, k);
-
-                    if (sum * m >= Math.pow(k, 2) + Math.pow(k+1, 2)) {
+                for (int k = 1; k <= 2 * (n - 1); k++) {
+                    int currX = i - k;
+                    int currY = j;
+                    for (int dir = 0; dir < 4; dir++) {
+                        for (int step = 0;  step < k; step++) {
+                            if (
+                                0 <= currX && currX < n
+                                && 0 <= currY && currY < n
+                            ) {
+                                sum += grid[currX][currY];
+                            }
+                            
+                            currX += dx[dir];
+                            currY += dy[dir];
+                        }
+                    }
+                    if (sum * m >= Math.pow(k,2) + Math.pow(k+1, 2)) {
                         max = Math.max(max, sum);
                     }
                 }
@@ -56,7 +43,5 @@ public class Main {
         }
 
         System.out.println(max);
-
-
     }
 }
