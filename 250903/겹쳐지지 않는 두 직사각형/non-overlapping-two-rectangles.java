ef962ff849117e1528bb4e43359c3380de0f34d0
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-09-03 11:14:17 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
+++ 250903/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -1,77 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static int[][] grid;
-
-    static int getSum(int i, int j, int k, int l) {
-
-        int sum = 0;
-
-        for (int a = i; a <= i + k; a++) {
-            for (int b = j; b <= j+l; b++) {
-                if (
-                    0 <= a &&  a < grid.length
-                    && 0 <= b && b < grid.length
-                ) {
-                    sum += grid[a][b];
-                }
-                else {
-                    return Integer.MIN_VALUE;
-                }
-            }
-        }
-
-        return sum;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        
-        grid = new int[n][m];
+        int[][] grid = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 grid[i][j] = sc.nextInt();
             }
         }
-
         // Please write your code here.
+
         int max = grid[0][0] + grid[0][1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                for (int k = 0; k < n; k++) {
-                    for (int l = 0; l < m; l++) {
+        for (int r1 = 0; r1 < n; r1++) {
+            for (int c1 = 0; c1 < m; c1++) {
+                for (int r2 = r1; r2 < n; r2++) {
+                    for (int c2 = c1; c2 < m; c2++) {
 
-                        for (int i2 = 0; i2 < n; i2++) {
-                            for (int j2 = 0; j2 < m; j2++) {
-                                for (int k2 = 0; k2 < n; k2++) {
-                                    for (int l2 = 0; l2 < m; l2++) {
-                                        if (
-                                            i + k < n && j + l < m
-                                            && (i + k < i2 || j + l < j2)
-                                        ) {
-                                            int sum1 = getSum(i, j, k, l);
-                                            int sum2 = getSum(i2, j2, k2, l2);
-                                            if (
-                                                sum1 != Integer.MIN_VALUE && sum2 != Integer.MIN_VALUE
-                                            ) {
-                                                max = Math.max(max, sum1 + sum2);
+                        for (int r3 = 0; r3 < n; r3++) {
+                            for (int c3 = 0; c3 < m; c3++) {
+                                for (int r4 = r3; r4 < n; r4++) {
+                                    for (int c4 = c3; c4 < m; c4++) {
+                                        if (r2 < r3 || c2 < c4) {
+                                            int sum = 0;
+
+                                            for (int a = r1; a <= r2; a++) {
+                                                for (int b = c1; b <= c2; b++) {
+                                                    sum += grid[a][b];
+                                                }
                                             }
-                                        }                                                                              
-                                        
+                                            for (int a = r3; a <= r4; a++) {
+                                                for (int b = c3; b <= c4; b++) {
+                                                    sum += grid[a][b];
+                                                }
+                                            }
+                                            max = Math.max(max, sum);                             
+                                        } else {
+                                            break;
+                                        }
                                     }
                                 }
+
                             }
                         }
-
-                    }
+                    }                           
                 }
             }
         }
-
         System.out.println(max);
     }
 }
