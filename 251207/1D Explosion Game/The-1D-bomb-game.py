41d7ba711564592588b4b3531a23c4e1c01863b7
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-12-07 03:21:37 +0900

    Title: 1D Explosion Game, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251207/1D Explosion Game/The-1D-bomb-game.py
+++ 251207/1D Explosion Game/The-1D-bomb-game.py
@@ -1,27 +1,24 @@
-n, m = map(int, input().split())
+n, m  = map(int, input().split())
 numbers = [int(input()) for _ in range(n)]
 
-preCount = n
 isFirst = True
+preCount = n
 
-while isFirst or preCount != len(numbers):
+while isFirst or preCount != len(numbers) :
     isFirst = False
     preCount = len(numbers)
     i = 0
     
-    while i < len(numbers):
-        consecution = 1
-        # 연속된 같은 숫자 개수 세기
-        while i + consecution < len(numbers) and numbers[i] == numbers[i + consecution]:
-            consecution += 1
-        
-        # m개 이상 연속이면 제거
-        if consecution >= m:
-            del numbers[i:i + consecution]
-        else:
+    while i < len(numbers) :
+        consecutation = 1
+        while i + consecutation < len(numbers) and numbers[i] == numbers[i + consecutation] :
+            consecutation += 1
+
+        if consecutation >= m :
+            del numbers[i : i + consecutation]
+        else :
             i += 1
 
-# 결과 출력
 print(len(numbers))
-for num in numbers:
+for num in numbers : 
     print(num)
