1ee36922610f160ecf2f1e294987e7f7544f3e8c
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-12-07 19:08:42 +0900

    Title: A Single Attempt at 2048, Time: 77ms, Memory: 18MB, Status: Passed - Codetree

--- 251207/A Single Attempt at 2048/one-trial-of-2048-game.py
+++ 251207/A Single Attempt at 2048/one-trial-of-2048-game.py
@@ -1,22 +1,88 @@
-n = 4
 grid = [
     list(map(int, input().split()))
-    for _ in range(n)
-]
-next_grid = [
-    [0 for _ in range(n)]
     for _ in range(4)
 ]
+dir = input()
+
+dir_num = {
+    'D' : 0,
+    'R' : 1,
+    'U' : 2,
+    'L' : 3
+}
+
+# print('\n'.join(map(str, grid)))
 
 def rotate():
-    for i in range(n):
-        for j in range(n):
-            next_grid[i][j] = 0
+    global grid
 
-    for i, row in  enumerate(grid):
-        for j in range(n):
-            next_grid[j][n - i - 1] = row[j]
-print('\n'.join(map(str, grid)))    
-rotate()
-print('\n'.join(map(str, next_grid)))
+    next_grid = [
+        [0, 0, 0, 0] for _ in range(4)
+    ]
+
+    for i, row in enumerate(grid):
+        for j in range(4):
+            next_grid[j][4 - 1 - i] = row[j]
+
+    grid = next_grid
+
+# rotate()
+# print('\n'.join(map(str, grid)))
+
+def drop():
+    next_grid = [
+        [0, 0, 0, 0] for _ in range(4)
+    ]
+
+    for j in range(4):
+        global grid
+        next_row, curr_num = 3, None
+        for i in range(3, -1, -1):
+            if grid[i][j] is 0:
+                continue
             
+            if curr_num is None:
+                curr_num = grid[i][j]
+
+            elif curr_num is grid[i][j]:
+                next_grid[next_row][j] = curr_num * 2
+                curr_num = None
+                next_row -= 1
+                
+            else:
+                next_grid[next_row][j] = curr_num
+                curr_num = grid[i][j]
+                next_row -= 1   
+
+        if curr_num is not None:
+            next_grid[next_row][j] = curr_num
+            curr_num = None
+            next_row -= 1
+        
+    grid = next_grid
+
+# drop()
+# print('\n'.join(map(str, grid)))
+
+def tilt(rotate_count):
+    for _ in range(rotate_count):
+        rotate()
+    
+    drop()
+
+    if rotate_count is not 0:
+        for _ in range(4 - rotate_count):
+            rotate()
+    
+
+
+tilt(dir_num[dir])
+for row in grid:
+    for num in row:
+        print(num, end=' ')
+    print()
+
+    
+
+
+        
