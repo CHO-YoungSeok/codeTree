7c49f967a2454225f1ac67b04e3da7a565cbd335
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-12-07 18:09:32 +0900

    Title: A Single Attempt at 2048, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251207/A Single Attempt at 2048/one-trial-of-2048-game.py
+++ 251207/A Single Attempt at 2048/one-trial-of-2048-game.py
@@ -1,9 +1,22 @@
-grid = [list(map(int, input().split())) for _ in range(4)]
-dir = str(input())
+n = 4
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+next_grid = [
+    [0 for _ in range(n)]
+    for _ in range(4)
+]
 
-print('\n'.join(map(str, grid)))
+def rotate():
+    for i in range(n):
+        for j in range(n):
+            next_grid[i][j] = 0
 
-if dir == "R" :
-
-elif dir == "L" :
-    
+    for i, row in  enumerate(grid):
+        for j in range(n):
+            next_grid[j][n - i - 1] = row[j]
+print('\n'.join(map(str, grid)))    
+rotate()
+print('\n'.join(map(str, next_grid)))
+            
