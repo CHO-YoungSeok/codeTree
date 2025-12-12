603e3b288532269d7afd65b1505e865c2a42334f
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-12-12 17:50:51 +0900

    Title: Conveyor Belt, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 251212/Conveyor Belt/conveyor-belt.py
+++ 251212/Conveyor Belt/conveyor-belt.py
@@ -4,12 +4,19 @@ up = list(map(int, input().split()))
 down = list(map(int, input().split()))
 
 belt = up + down
+# print(belt)
 
 while 0 < t:
-    temp = belt[2 * n - 1]
-    for i in range(2 * n - 1, -1, -1):
+    temp = belt[n * 2 - 1]
+    for i in range(n * 2 -1, -1, -1):
         belt[i] = belt[i - 1]
     
     belt[0] = temp
+    t -= 1
 
-print(belt)
+
+up = belt[:n]
+down = belt[n:]
+
+print(' '.join(map(str, up)))
+print(' '.join(map(str, down)))
