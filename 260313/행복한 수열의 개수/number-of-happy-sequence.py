5c2a2b8d83ecea7ebbdbba030bb98a92bb114349
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2026-03-13 14:46:50 +0900

    Title: 행복한 수열의 개수, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 260313/행복한 수열의 개수/number-of-happy-sequence.py
+++ 260313/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,37 +1,40 @@
-import sys
-
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
+# Please write your code here.
+
 count_happy = 0
-for i in range(n):
-    max_seq = 1
-    seq = 1
-
-    for c in range(n - 1):
-        if grid[i][c+1] == grid[i][c]:
-           seq += 1
-        else:
-            seq = 1
-        
-        max_seq = max(seq, max_seq)
-        
-    if max_seq >= m:
-        count_happy += 1
-
-for i in range(n):
-    max_seq = 1
-    seq = 1
-    for r in range(n - 1):
-        if grid[r + 1][i] == grid[r][i]:
-           seq += 1
-        else:
-            seq = 1
-        
-        max_seq = max(seq, max_seq)
-        
-    if max_seq >= m:
-        count_happy += 1
+for r in range(n):
+    max_cnum = 1
+    cnum = 1  
     
+    for c in range(n-1):
+        if grid[r][c] == grid[r][c+1]:
+            cnum += 1
+        else:
+            cnum = 1
+        
+        max_cnum = max(max_cnum, cnum)
+    
+    if max_cnum >= m:
+        count_happy += 1
+
+for c in range(n):
+    max_cnum = 1
+    cnum = 1  
+    
+    for r in range(n-1):
+        if grid[r + 1][c] == grid[r][c]:
+            cnum += 1
+        else:
+            cnum = 1
+        
+        max_cnum = max(max_cnum, cnum)
+    
+    if max_cnum >= m:
+        count_happy += 1
 
 print(count_happy)
+
+            
+        
