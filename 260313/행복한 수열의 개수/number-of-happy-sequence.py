51933eadc19c446b27031d5cd41aa8340532487a
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2026-03-13 14:36:36 +0900

    Title: 행복한 수열의 개수, Time: 79ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260313/행복한 수열의 개수/number-of-happy-sequence.py
+++ 260313/행복한 수열의 개수/number-of-happy-sequence.py
@@ -7,26 +7,30 @@ def log(msg):
     # print(msg)
     pass
 
-if n == 1 or m == 1:
-    print(2 * n)
-    sys.exit()
+# if n == 1 or m == 1:
+#     print(2 * n)
+#     sys.exit()
     
 
 count_happy = 0
 for i in range(n):
+    max_seq = 1
     seq = 1
+
     for c in range(n - 1):
         if grid[i][c+1] == grid[i][c]:
            seq += 1
         else:
             seq = 1
         
-        if seq >= m:
-            count_happy += 1
-            log(f'col {i}, {c}')
-            break
+        max_seq = max(seq, max_seq)
+        
+    if max_seq >= m:
+        count_happy += 1
+        log(f'col {i}, {c}')
 
 for i in range(n):
+    max_seq = 1
     seq = 1
     for r in range(n - 1):
         if grid[r + 1][i] == grid[r][i]:
@@ -34,10 +38,11 @@ for i in range(n):
         else:
             seq = 1
         
-        if seq >= m:
-            count_happy += 1
-            log(f'row {r}, {i}')
-            break
+        max_seq = max(seq, max_seq)
+        
+    if max_seq >= m:
+        count_happy += 1
+        log(f'row {r}, {i}')
     
 
 print(count_happy)
