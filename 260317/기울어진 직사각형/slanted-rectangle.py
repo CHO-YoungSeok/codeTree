67df5e7d8b51b7dcb29bfeb35be47e28376760b1
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2026-03-17 01:21:39 +0900

    Title: 기울어진 직사각형, Time: 55ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260317/기울어진 직사각형/slanted-rectangle.py
+++ 260317/기울어진 직사각형/slanted-rectangle.py
@@ -2,56 +2,40 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-
-
 max_sum = 0
-dr = [-1, -1, 1, 1]
-dc = [1, -1, -1, 1]
+
+def log(msg):
+    # print(msg)
+    pass
+
+def get_sum(r, c, w, h) -> int:
+    dr = [-1, -1, 1, 1]
+    dc = [1, -1, -1, 1]
+    move_len = [w, h, w, h]
+    sum = 0
+
+    for dir in range(4):
+        for ww in range(w):
+            for hh in range(h):
+                r += dr[dir]
+                c += dc[dir]
+                if 0 <= r and r < n and 0 <= c and c < n:
+                    sum += grid[r][c]
+                else:
+                    return -1
+    
+    return sum
+
+
 
 for r in range(n):
     for c in range(n):        
         for w in range(1, n):
             for h in range(1, n):
-                sum = 0
-                rr = r
-                cc = c
-                is_valid = True
-                for ww in range(w):
-                    rr += dr[0]
-                    cc += dc[0]
-                    if cc < n and rr < n and 0<= cc and 0<= rr:
-                        sum += grid[rr][cc]
-                    else:
-                        is_valid = False
-                        break
-                for hh in range(h):
-                    rr += dr[1]
-                    cc += dc[1]
-                    if cc < n and rr < n and 0<= cc and 0 <= rr:
-                        sum += grid[rr][cc]
-                    else:
-                        is_valid = False
-                        break
-                for ww in range(w):
-                    rr += dr[2]
-                    cc += dc[2]
-                    if cc < n and rr < n and 0<= cc and 0<= rr:
-                        sum += grid[rr][cc]
-                    else:
-                        is_valid = False
-                        break
-                for hh in range(h):
-                    rr += dr[3]
-                    cc += dc[3]
-                    if cc < n and rr < n and 0<= cc and 0 <= rr:
-                        sum += grid[rr][cc]
-                    else:
-                        is_valid = False
-                        break
-
-                if is_valid and max_sum < sum:
-                    # print(f'{r} {c} {w} {h}')
-                    max_sum = max(max_sum, sum)
+                sum = get_sum(r, c, w, h)
+                if max_sum < sum:
+                    log(f'{r} {c} {w} {h}')
+                max_sum = max(max_sum, sum)
 
 
 
