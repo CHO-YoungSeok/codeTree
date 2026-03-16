a8a9affcbdd68b871c7ef330845a007ea695ab5e
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2026-03-16 16:06:22 +0900

    Title: 트로미노, Time: 78ms, Memory: 17MB, Status: Passed - Codetree

--- 260316/트로미노/tromino.py
+++ 260316/트로미노/tromino.py
@@ -2,88 +2,37 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-
-shape = [
-    [
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-    ],
-    [
-        [1, 1, 1],
-        [0, 0, 0],
-        [0, 0, 0],
-    ],
-    [
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 0, 0],
-    ],
-    [
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 0, 0],
-    ],
-    [
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 0, 0],
-    ],
-    [
-        [1, 1, 0],
-        [1, 0, 0],
-        [0, 0, 0],
-    ],
-]
-
-di_x = [
-    [-1, -1, -1],
-    [0, 0, 0],
-    [1, 1, 1],
-]
-di_y = [
-    [-1, 0, 1],
-    [-1, 0, 1],
-    [-1, 0, 1],
-]
-
 def log(msg):
     # print(msg)
     pass
+
+shapes = [
+    [(0,0), (0,1), (0,2)],
+    [(0,0), (1,0), (2,0)],
+
+    [(0,0), (1,0), (1,1)],
+    [(0,1), (1,0), (1,1)],
+    [(0,0), (0,1), (1,1)],
+    [(0,0), (1,0), (0,1)],
+]
 
 max_sum = 0
 
 for r in range(n):
     for c in range(m):
-        for shape_i in range(len(shape)):
+        for shape in shapes:
             sum = 0
-            is_valid = True
-            for xi in range(3):
-                for yi in range(3):
-                    xx = r + xi
-                    yy = c + yi
-                    if (shape[shape_i][xi][yi] == 1):
-                        if xx >= n or yy >= m :
-                            is_valid = False
-                            continue
-
-                        sum += grid[xx][yy]
-
-            if is_valid == True and max_sum < sum:
+            for dr, dc in shape:
+                rr = r + dr
+                cc = c + dc
+                if 0 <= rr and rr < n and 0 <= cc and cc < m:
+                    sum += grid[rr][cc]
+                else:
+                    sum = -1
+                    break
+            
+            if max_sum < sum:
                 max_sum = sum
-                log(f'{r} {c} {sum}')
-
+                log(f'{r} {c} {max_sum}')
 
 print(max_sum)
-
-
-
-            
-                    
-                    
-                    
-
-                    
-
-
-
