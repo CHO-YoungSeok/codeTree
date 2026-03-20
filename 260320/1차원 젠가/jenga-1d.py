53990342f1b803f5d4397e0c544084dd64122a12
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2026-03-20 13:44:59 +0900

    Title: 1차원 젠가, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 260320/1차원 젠가/jenga-1d.py
+++ 260320/1차원 젠가/jenga-1d.py
@@ -4,22 +4,8 @@ s1, e1 = map(int, input().split())
 s2, e2 = map(int, input().split())
 
 # Please write your code here.
-def act(s, e):
-    s -= 1
-    e -= 1
-    global blocks
-    temp_arr = []
-    for i in range(len(blocks)):
-        if s <= i and i <= e:
-            continue
-        
-        temp_arr.append(blocks[i])
-    blocks = list(temp_arr)
-
-act(s1, e1)
-act(s2, e2)
-
+blocks = blocks[:s1-1] + blocks[e1:]
+blocks = blocks[:s2-1] + blocks[e2:]
 print(len(blocks))
-
 for num in blocks:
     print(num)
