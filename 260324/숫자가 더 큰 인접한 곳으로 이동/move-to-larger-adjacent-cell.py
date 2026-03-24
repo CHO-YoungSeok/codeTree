857aa1f5a658d7e6ede60a069af4e58b1f830f7d
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2026-03-24 17:01:53 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 71ms, Memory: 18MB, Status: Passed - Codetree

--- 260324/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 260324/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,15 +1,13 @@
 n, r, c = map(int, input().split())
-a = [[0] * (n + 1) for _ in range(n + 1)]
+a = [[0] * (n) for _ in range(n)]
 
 for i in range(1, n + 1):
     row = list(map(int, input().split()))
     for j in range(1, n + 1):
-        a[i][j] = row[j - 1]
+        a[i- 1][j- 1] = row[j - 1]
 
 # Please write your code here.
 import copy
-import collections
-
 grid = copy.deepcopy(a)
 
 dr = [-1, 1, 0, 0]
@@ -27,6 +25,8 @@ def move_to_bigger(r, c) -> tuple[bool, int, int]:
     
     return (False, 0, 0)
 
+r -= 1
+c -= 1
 history = []
 while True:
     history.append(grid[r][c])
